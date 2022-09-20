59a3511c9e960bf6acd11f6d557151dd73101fce
sobiya <sarockia@asu.edu>
2022-09-20 19:46:54 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import './App.css';
+import { Route, Routes, Navigate } from "react-router-dom";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
 
 function App() {
-  return (
-  <>
-  hello world
-  
-  </>
-  );
+	const user = localStorage.getItem("token");
+
+	return (
+		<Routes>
+			{user && <Route path="/dashboard" exact element={<Dashboard />} />}
+			<Route path="/register" exact element={<Register />} />
+			<Route path="/login" exact element={<Login />} />
+			<Route path="/" element={<Navigate replace to="/login" />} />
+		</Routes>
+	);
 }
 
 export default App;
