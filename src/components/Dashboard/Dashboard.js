245241939611ec096094fa91e51d44102691595b
Elizabeth Sobiya <98687741+ElizabethSobiya@users.noreply.github.com>
2022-09-21 10:32:51 +0530

    Update Dashboard.js

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,12 +1,12 @@
 import styles from "./style.module.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+	const navigate = useNavigate()
 	const handleLogout = () => {
 		localStorage.removeItem("token");
-		window.location.reload();
+		navigate('/');
 	};
-
 	return (
 		<div className={styles.main_container}>
 			<nav className={styles.navbar}>
